777baa71386bee8b1a188eb56d7b039ebb50cde3
Dave Lewis <siu06dl@gmail.com>
2025-10-31 17:34:30 +0000

    fix: return transaction id

--- ECommerceAPI/Models/DTOs/ProductSaleDto.cs
+++ ECommerceAPI/Models/DTOs/ProductSaleDto.cs
@@ -2,6 +2,7 @@ namespace ECommerceAPI.Models.DTOs;
 
 public class ProductSaleDto
 {
+    public int ID { get; set; }
     public int ProductId { get; set; }
     public int SaleId { get; set; }
     public int Quantity { get; set; }
@@ -9,6 +10,7 @@ public class ProductSaleDto
     public static ProductSaleDto FromProductSale(ProductSale productSale) =>
         new()
         {
+            ID = productSale.ID,
             ProductId = productSale.ProductID,
             SaleId = productSale.SaleID,
             Quantity = productSale.Quantity,
