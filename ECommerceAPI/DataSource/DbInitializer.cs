50cccd6a45d39829005a2701484e5c898dcdbc8b
Dave Lewis <siu06dl@gmail.com>
2025-10-31 17:05:12 +0000

    feat: soft delete sales

--- ECommerceAPI/DataSource/DbInitializer.cs
+++ ECommerceAPI/DataSource/DbInitializer.cs
@@ -13,9 +13,9 @@ public class DbInitializer
 
         var products = new Product[]
         {
-            new Product { Name = "Shadow Alchemist: Alchemy in the Shadows", Price = new decimal(3.50d) },
-            new Product { Name = "F-You Earth!", Description = "My second game jam entry", Price = new decimal(4.20d) },
-            new Product { Name = "Spacelancer", Description = "My nemesis", Price = new decimal(6.40d), Deleted = true },
+            new() { Name = "Shadow Alchemist: Alchemy in the Shadows", Price = new decimal(3.50d) },
+            new() { Name = "F-You Earth!", Description = "My second game jam entry", Price = new decimal(4.20d) },
+            new() { Name = "Spacelancer", Description = "My nemesis", Price = new decimal(6.40d), Deleted = true },
         };
 
         context.Products.AddRange(products);
@@ -23,8 +23,9 @@ public class DbInitializer
 
         var sales = new Sale[]
         {
-            new Sale { FirstName = "John", LastName = "Doe" },
-            new Sale { FirstName = "Jane", LastName = "Doe" },
+            new() { FirstName = "John", LastName = "Doe" },
+            new() { FirstName = "Jane", LastName = "Doe" },
+            new() { FirstName = "Deleted", LastName = "McDeletyFace", Deleted = true },
         };
         
         context.Sales.AddRange(sales);
@@ -32,9 +33,9 @@ public class DbInitializer
 
         var productSales = new ProductSale[]
         {
-            new ProductSale { ProductID = 1, Quantity = 1, SaleID = 1 },
-            new ProductSale { ProductID = 2, Quantity = 2, SaleID = 2 },
-            new ProductSale { ProductID = 1, Quantity = 1, SaleID = 2 },
+            new() { ProductID = 1, Quantity = 1, SaleID = 1 },
+            new() { ProductID = 2, Quantity = 2, SaleID = 2 },
+            new() { ProductID = 1, Quantity = 1, SaleID = 2 },
         };
         
         context.ProductSales.AddRange(productSales);
