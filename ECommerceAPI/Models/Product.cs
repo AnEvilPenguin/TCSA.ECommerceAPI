aae50890897e79256ae00c00e3b261ddf25dcd93
Dave Lewis <siu06dl@gmail.com>
2025-11-30 14:47:45 +0000

    refactor: remove required string attributes
    We need this for excel parsing right now
    I'm sure there's a better way of handling this but for the moment it's not important

--- ECommerceAPI/Models/Product.cs
+++ ECommerceAPI/Models/Product.cs
@@ -3,7 +3,7 @@ namespace ECommerceAPI.Models;
 public class Product
 {
     public int ID { get; set; }
-    public required string Name { get; set; }
+    public string Name { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; }
     public bool Deleted { get; set; }
--- ECommerceAPI/Models/Sale.cs
+++ ECommerceAPI/Models/Sale.cs
@@ -3,8 +3,8 @@ namespace ECommerceAPI.Models;
 public class Sale
 {
     public int ID { get; set; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public bool Deleted { get; set; }
     public ICollection<ProductSale>? ProductSales { get; set; }
 }
